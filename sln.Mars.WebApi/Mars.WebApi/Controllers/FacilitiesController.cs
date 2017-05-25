6e12c80408865159c7f7acfab3ebadd44e5be96f
Chen, Dong <raycdut@hotmail.com>
2017-05-25 21:15:24 +0800

    add api description

---
+++ sln.Mars.WebApi/Mars.WebApi/Controllers/FacilitiesController.cs
@@ -0,0 +1,55 @@
+﻿using Mars.Entities;
+using Mars.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Mars.WebApi.Controllers
+{
+    /// <summary>
+    /// Get Facility related information
+    /// </summary>
+    public class FacilitiesController : ApiController
+    {
+        private IFacilityService FacilityService;
+        public FacilitiesController(IFacilityService _FacilityService)
+        {
+            this.FacilityService = _FacilityService;
+
+        }
+        /// <summary>
+        /// Get All facility Information
+        /// </summary>
+        /// <returns></returns>
+        // GET api/values
+        public IEnumerable<Facility> Get()
+        {
+            return FacilityService.GetFacilities();
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
